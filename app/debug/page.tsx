afc360d0a9fb2c23d6a42486db8df909171b97a2
pro <pro@pros-MacBook-Pro.local>
2026-01-18 23:36:26 +0500

    fix: use configured prisma client in debug page

--- app/debug/page.tsx
+++ app/debug/page.tsx
@@ -1,7 +1,5 @@
 
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
+import { prisma } from "../lib/prisma";
 
 export default async function DebugPage() {
     // 1. Check Env
